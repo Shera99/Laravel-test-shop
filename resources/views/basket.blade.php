cc5ac1ebf583c1332638aa0ff68d79d78f2bf762
Shera99 <shera.mamatbekov@gmail.com>
2021-07-11 12:03:48 +0600

    Global refactors from youtube work video part 3
    Signed-off-by: Shera99 <shera.mamatbekov@gmail.com>

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -18,32 +18,36 @@
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>
-                        <a href="http://internet-shop.tmweb.ru/mobiles/iphone_x_256">
-                            <img height="56px" src="http://internet-shop.tmweb.ru/storage/products/iphone_x_silver.jpg">
-                            iPhone X 256GB
-                        </a>
-                    </td>
-                    <td><span class="badge">1</span>
-                        <div class="btn-group form-inline">
-                            <form action="http://internet-shop.tmweb.ru/basket/remove/2" method="POST">
-                                <button type="submit" class="btn btn-danger" href=""><span
-                                        class="glyphicon glyphicon-minus" aria-hidden="true"></span></button>
-                                <input type="hidden" name="_token" value="BAloGJy8KUPgfAMivTdLPuyLEgM534uFzFRDlzHM">                            </form>
-                            <form action="http://internet-shop.tmweb.ru/basket/add/2" method="POST">
-                                <button type="submit" class="btn btn-success"
-                                        href=""><span
-                                        class="glyphicon glyphicon-plus" aria-hidden="true"></span></button>
-                                <input type="hidden" name="_token" value="BAloGJy8KUPgfAMivTdLPuyLEgM534uFzFRDlzHM">                            </form>
-                        </div>
-                    </td>
-                    <td>89990 ₽</td>
-                    <td>89990 ₽</td>
-                </tr>
+                @foreach($order->products as $product)
+                    <tr>
+                        <td>
+                            <a href="{{ route('product', [$product->category->code, $product->code]) }}">
+                                <img height="56px" src="http://internet-shop.tmweb.ru/storage/products/iphone_x_silver.jpg">
+                                {{ $product->name }}
+                            </a>
+                        </td>
+                        <td><span class="badge">1</span>
+                            <div class="btn-group form-inline">
+                                <form action="{{ route('basket-add', $product) }}" method="POST">
+                                    <button type="submit" class="btn btn-danger" href=""><span
+                                            class="glyphicon glyphicon-minus" aria-hidden="true"></span></button>
+                                    @csrf
+                                </form>
+                                <form action="{{ route('basket-add', $product) }}" method="POST">
+                                    <button type="submit" class="btn btn-success"
+                                            href=""><span
+                                            class="glyphicon glyphicon-plus" aria-hidden="true"></span></button>
+                                    @csrf
+                                </form>
+                            </div>
+                        </td>
+                        <td>{{ $product->price }} ₽</td>
+                        <td>{{ $product->price }} ₽</td>
+                    </tr>
+                @endforeach
                 <tr>
                     <td colspan="3">Общая стоимость:</td>
-                    <td>89990 ₽</td>
+                    <td>{{ $order->products->sum('price') }} ₽</td>
                 </tr>
                 </tbody>
             </table>
--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -9,12 +9,12 @@
             <h3>{{ $product->name }}</h3>
             <p>{{ $product->price }} ₽</p>
             <p>
-            <form action="{{ route('basket') }}" method="POST">
+            <form action="{{ route('basket-add', $product) }}" method="POST">
                 <button type="submit" class="btn btn-primary" role="button">В корзину</button>
                 <a href="{{ route('product', [$product->category->code, $product->code]) }}"
                    class="btn btn-default"
                    role="button">Подробнее</a>
-                <input type="hidden" name="_token" value="qC2icB2OibxxDS3HCMsBySUr1M5KIfDeQUeygdCH">
+                @csrf
             </form>
             </p>
         </div>
